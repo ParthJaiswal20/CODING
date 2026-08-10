73fa8c47fe140a3538b21916b2a6ee67165064b8
Vaibhav Jaiswal <parthjaiswal792@gmail.com>
2026-08-10 07:49:44 +0530

    Time: 0 ms (100%), Space: 28.3 MB (42.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,30 @@
+//          USING MOORES VOTING ALGORITHM
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mpp;
-        int outer;
         int size = nums.size();
-        int target = size / 2;
+        int count = 0;
+        int el;
+        int outer;
 
-        for (int x : nums) {
-            mpp[x]++;
-        }
-
-        for (auto it : mpp) {
-            if (it.second > target) {
-                return it.first;
+        for (outer = 0; outer < size; outer++) {
+            if (count == 0) {
+                count = 1;
+                el = nums[outer];
+            } else if (nums[outer] == el) {
+                count++;
+            } else {
+                count--;
             }
+        }
+        int count1 = 0;
+        for (outer = 0; outer < size; outer++) {
+            if (nums[outer] == el) {
+                count1++;
+            }
+        }
+        if (count1 > size / 2) {
+            return el;
         }
         return -1;
     }
