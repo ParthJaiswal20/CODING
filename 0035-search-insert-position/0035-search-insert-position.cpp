8a07afa1bae39b1959124b3f118c790439604d4d
Vaibhav Jaiswal <parthjaiswal792@gmail.com>
2026-08-11 21:15:24 +0530

    Time: 0 ms (100%), Space: 13.7 MB (44.09%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,3 +1,4 @@
+            // THIS IS DONE USING THE LOWER BOUND 
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
@@ -5,17 +6,17 @@ public:
         int outer;
         int high = size - 1;
         int low = 0;
+        int ans;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid-1;
             } else {
                 low = mid + 1;
             }
         }
-        return high + 1;
+        return ans;
     }
 };
